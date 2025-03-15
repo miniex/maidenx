5d2fbbc2864d4c45c0e3bb9ddc912d650091671b
Han Damin <miniex@daminstudio.net>
2025-03-16 00:12:18 +0900

    fix: nn::functional -> nn::alias

--- crates/maidenx_nn/src/lib.rs
+++ crates/maidenx_nn/src/lib.rs
@@ -1,12 +1,13 @@
 pub mod layer;
 pub mod optimizer;
 
-pub mod functional;
+pub mod alias;
 pub mod layers;
 pub mod losses;
 pub mod optimizers;
 
 pub use crate::{
+    alias::*,
     layers::linear::Linear,
     losses::{huber::Huber, mae::MAE, mse::MSE},
     optimizers::{adam::Adam, sgd::SGD},
