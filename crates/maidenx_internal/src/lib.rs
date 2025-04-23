0817be99d9f598739c319a51395af5d114b60cae
Han Damin <miniex@daminstudio.net>
2025-04-23 18:48:57 +0900

    bump: update version from 0.1.0-dev to 0.2.0-dev

--- crates/maidenx_internal/src/lib.rs
+++ crates/maidenx_internal/src/lib.rs
@@ -3,5 +3,6 @@ pub mod prelude;
 pub use maidenx_core as core;
 pub use maidenx_nn as nn;
 pub use maidenx_tensor as tensor;
+pub use maidenx_tensor_v2 as tensor_v2;
 
 pub use maidenx_core::dtype::*;
